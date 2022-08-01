cb3813307b915fd8b5eae81336b856b49b51e42f
llyri <103306857+llyri@users.noreply.github.com>
2022-08-01 17:39:02 +0900

    arrow function

---
+++ 국기코딩/jquery/자바스크립트/함수/반응형/js/main.js
@@ -0,0 +1,5 @@
+let grid=new Isotope("section",{ //배치할 요소를 감싸고 있는 부모요소
+    itemSelector:"article", //배치할 요소명
+    columnWidth:"article", //넓이값을 구할 요소명
+    transitionDuration:"0.5s"//화면 재배치시 요소가 움직이는 속도
+});
