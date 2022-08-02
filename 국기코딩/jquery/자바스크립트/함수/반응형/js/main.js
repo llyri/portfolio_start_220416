85e9e63c701ce899b1a6faeecff77f733ed20f8a
llyri <103306857+llyri@users.noreply.github.com>
2022-08-02 14:07:29 +0900

    javascript

--- 국기코딩/jquery/자바스크립트/함수/반응형/js/main.js
+++ 국기코딩/jquery/자바스크립트/함수/반응형/js/main.js
@@ -1,5 +1,26 @@
-let grid=new Isotope("section",{ //배치할 요소를 감싸고 있는 부모요소
-    itemSelector:"article", //배치할 요소명
-    columnWidth:"article", //넓이값을 구할 요소명
-    transitionDuration:"0.5s"//화면 재배치시 요소가 움직이는 속도
+let grid = new Isotope("section", { //배치할 요소를 감싸고 있는 부모요소
+    itemSelector: "article", //배치할 요소명
+    columnWidth: "article", //넓이값을 구할 요소명
+    transitionDuration: "0.7s"//화면 재배치시 요소가 움직이는 속도
 });
+
+let btns = document.querySelectorAll(".menu li")
+
+for (let el of btns) {
+    el.addEventListener("click", (e) => {
+        e.preventDefault();
+        let sort = e.currentTarget.querySelector("a").getAttribute("href");
+
+        // plugIn
+        // grid 안에 저장된 결과를 불러와 재정렬 기능을 연결
+        grid.arrange({
+            filter: sort
+        });
+
+        for (let el of btns) {
+            el.classList.remove("on")
+        }
+
+        e.currentTarget.classList.add("on");
+    })
+}
